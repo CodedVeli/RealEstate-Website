59d8da6a855c3432beb87beabb293de739f4b1eb
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2024-01-24 14:00:49 +0300

    Updated About and Contact

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import "./Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  const ContactUs =
+    "https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  return (
+    <div className="contact">
+      <div className="contactimage">
+        <img className="contimage" src={ContactUs} alt="" />
+        <h1 className="contacttext">Contact Us</h1>
+      </div>
+
+      <div className="contactform">
+        <h1>Contact Us</h1>
+        <form>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+      <div className="footer">
+        <div className="contact1">
+          <h1>Contact</h1>
+          <p>Westlands,Nairobi Kenya</p>
+          <p>P.O Box 20227 - 00100</p>
+          <p>Nairobi Kenya</p>
+          <p>+254700000000</p>
+        </div>
+        <div className="connect">
+          <h1>Connect</h1>
+          <div className="ic">
+            <h1>
+              <FaInstagram />
+            </h1>
+            <h1>
+              <FaSquareXTwitter />
+            </h1>
+            <h1>
+              <FaFacebook />
+            </h1>
+          </div>
+        </div>
+        <div className="explore">
+          <h1>Explore</h1>
+          <p>For Sale</p>
+          <p>For Rent</p>
+          <p>News</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
