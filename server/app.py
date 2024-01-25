e9d98e90c00f9f1f2c34685c604639cd72404d7c
eric githaiga <ericgithaiga007@gmail.com>
2024-01-26 00:33:38 +0300

    model adjustment

--- server/app.py
+++ server/app.py
@@ -1,105 +1,114 @@
-from flask import Flask, make_response, jsonify, request, session
-from flask_migrate import Migrate
-from flask_restful import Api, Resource, reqparse
-from models import db, User
-
+from flask import Flask, request, jsonify, session
+from itsdangerous import URLSafeTimedSerializer
+from flask_bcrypt import Bcrypt 
+from flask_cors import CORS, cross_origin
+from flask_restful import Api, Resource, reqparse 
+from models import db, User, Property
+ 
 app = Flask(__name__)
+ 
+app.config['SECRET_KEY'] = 'navasmuller-herbert'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+ 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+  
+bcrypt = Bcrypt(app) 
+CORS(app, supports_credentials=True)
 
-app.secret_key = b'ndchhacaewuhsncaxzxwxcdsac'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
 
 api = Api(app)
+db.init_app(app)
+  
+with app.app_context():
+    db.create_all()
+ 
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+ 
+@app.route("/signup", methods=["POST"])
+def signup():
+    name = request.json["name"]
+    email = request.json["email"]
+    password = request.json["password"]
+ 
+    user_exists = User.query.filter_by(email=email).first() is not None
 
-class Login(Resource):
-    def post(self):
-        user = User.query.filter(
-            User.username == request.get_json()['username']
-        ).first()
+ 
+    if user_exists:
+        return jsonify({"error": "Email already exists"}), 409
+     
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(name=name,email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+ 
+    session["user_id"] = new_user.id
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    token = s.dumps(new_user.email, salt='email-confirm')
+ 
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email,
+        "token" : token
+    })
 
-        session['user_id'] = user.id
-        return jsonify(user.to_dict())
-    
-class CheckSession(Resource):
-    def get(self):
-        user = User.query.filter(User.id == session.get('user_id')).first()
-        if user:
-            return jsonify(user.to_dict())
-        else:
-            return jsonify({'message': '401: Not Authorized'}), 401
-        
+
+
+ 
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+  
+    user = User.query.filter_by(email=email).first()
+  
+    if user is None:
+        return jsonify({"error": "Unauthorized Access"}), 401
+  
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+      
+    session["user_id"] = user.id
+  
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
+
 class Logout(Resource):
     def delete(self):
         session['user_id'] = None
         return jsonify({'message': '204: No Content'}), 204
-    
-class Property(Resource):
+ 
+class PropertyResource(Resource):
     def get(self, property_id=None):
         if property_id:
-            house_data = Property.query.filter_by(id=property_id).first()
-            if house_data:
-                return {
-                    'id': house_data.id,
-                    'name': house_data.name,
-                    'type': house_data.type,
-                    'user_id': house_data.user_id,
-                    'bedrooms': house_data.bedrooms,
-                    'bathrooms': house_data.bathrooms,
-                    'parking': house_data.parking,
-                    'furnished': house_data.furnished,
-                    'offer': house_data.offer,
-                    'regular_price': house_data.regular_price,
-                    'discounted_price': house_data.discounted_price,
-                    'location': house_data.location,
-                    'latitude': house_data.latitude,
-                    'longitude': house_data.longitude,
-                    'image_urls': house_data.image_urls
-                }
+            property_data = Property.query.filter_by(id=property_id).first()
+            if property_data:
+                return property_data.to_dict()
             else:
                 return {'message': 'Property not found'}, 404
         else:
             properties = Property.query.all()
-            return [{'id': prop.id, 'name': prop.name} for prop in properties]
+            return [prop.to_dict() for prop in properties]
 
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str, required=True)
-        parser.add_argument('type', type=str, required=True)
         parser.add_argument('user_id', type=int, required=True)
         parser.add_argument('bedrooms', type=int, required=True)
         parser.add_argument('bathrooms', type=int, required=True)
-        parser.add_argument('parking', type=bool, required=True)
-        parser.add_argument('furnished', type=bool, required=True)
-        parser.add_argument('offer', type=bool, required=True)
-        parser.add_argument('regular_price', type=float, required=True)
-        parser.add_argument('discounted_price', type=float, required=True)
+        parser.add_argument('price', type=float, required=True)
         parser.add_argument('location', type=str, required=True)
         parser.add_argument('latitude', type=float, required=True)
         parser.add_argument('longitude', type=float, required=True)
-        parser.add_argument('image_urls', type=str, action='append')
+        parser.add_argument('image', type=str, required=True)
         
         args = parser.parse_args()
         
-        new_property = Property(
-            name=args['name'],
-            type=args['type'],
-            user_id=args['user_id'],
-            bedrooms=args['bedrooms'],
-            bathrooms=args['bathrooms'],
-            parking=args['parking'],
-            furnished=args['furnished'],
-            offer=args['offer'],
-            regular_price=args['regular_price'],
-            discounted_price=args['discounted_price'],
-            location=args['location'],
-            latitude=args['latitude'],
-            longitude=args['longitude'],
-            image_urls=args['image_urls'],
-        )
+        new_property = Property(**args)
         
         db.session.add(new_property)
         db.session.commit()
@@ -110,19 +119,14 @@ class Property(Resource):
         if property_data:
             parser = reqparse.RequestParser()
             parser.add_argument('name', type=str)
-            parser.add_argument('type', type=str)
             parser.add_argument('user_id', type=int)
             parser.add_argument('bedrooms', type=int)
             parser.add_argument('bathrooms', type=int)
-            parser.add_argument('parking', type=bool)
-            parser.add_argument('furnished', type=bool)
-            parser.add_argument('offer', type=bool)
-            parser.add_argument('regular_price', type=float)
-            parser.add_argument('discounted_price', type=float)
+            parser.add_argument('price', type=float)
             parser.add_argument('location', type=str)
             parser.add_argument('latitude', type=float)
             parser.add_argument('longitude', type=float)
-            parser.add_argument('image_urls', type=str, action='append')
+            parser.add_argument('image', type=str)
             
             args = parser.parse_args()
 
@@ -144,10 +148,8 @@ class Property(Resource):
         else:
             return {'message': 'Property not found'}, 404
         
-api.add_resource(Property, '/properties', '/properties/<int:property_id>')
-api.add_resource(Login, '/login')
-api.add_resource(CheckSession, '/check_session')
+api.add_resource(PropertyResource, '/properties', '/properties/<int:property_id>')
 api.add_resource(Logout, '/logout')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5555)
+if __name__ == "__main__":
+    app.run(debug=True)
--- server/models.py
+++ server/models.py
@@ -1,33 +1,31 @@
-from flask import Flask
-from sqlalchemy import Integer, ForeignKey, Boolean, Float, String
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Base(db.Model):
+    __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
 
+class User(Base):
+    # role = db.Column(db.String(50), default='user')
+    properties = db.relationship('Property', backref='user', lazy=True)
+
+class Owner(Base):
+    properties = db.relationship('Property', backref='owner', lazy=True)
 
 class Property(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
     bedrooms = db.Column(db.Integer, nullable=False)
     bathrooms = db.Column(db.Integer, nullable=False)
-
     price = db.Column(db.Float, nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    image = db.Column(db.String(255),nullable=False)
-    
-    price = db.Column(db.Float, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     image = db.Column(db.String(255), nullable=False)
