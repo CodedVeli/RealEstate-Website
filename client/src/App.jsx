b34bd9d0b674a887d78fe124224ef55a0eeddb83
eric githaiga <ericgithaiga007@gmail.com>
2024-01-25 09:23:30 +0300

    edit lising implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -12,6 +14,7 @@ import "./App.css";
 function App() {
   return (
     <div>
+       <ToastContainer />  
       <Router>
         <Navbar />
         <Routes>
@@ -23,6 +26,7 @@ function App() {
           <Route path="/create" element={<CreateListing />} />
         </Routes>
       </Router>
+       
     </div>
   );
 }
